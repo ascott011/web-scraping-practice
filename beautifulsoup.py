667a5c6c868306b44df552e8b010935cbaa09b81
Alex Scott <ascott011uk@gmail.com>
2026-01-17 16:30:32 -0500

    successfully scrapes the data and creates lists of the column headers and table data. Second function puts those lists into a pandas df. Jupyter Notebook for testing step by step

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 
 # check the status code to start as a habit
@@ -11,17 +12,50 @@ headers = {
 
 url = 'https://www.nfl.com/stats/team-stats/'
 
-response = requests.get(url, headers=headers)
-if response.status_code != 200:
-    print("Failed to retrieve the webpage")
-    print(response.status_code)
-    exit()
-soup = BeautifulSoup(response.content, 'html.parser')
+def get_stats(url, headers):
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        print("Failed to retrieve the webpage")
+        print(response.status_code)
+        exit()
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-headers = soup.find("thead").text.splitlines()
-table_body = soup.find("tbody")
-rows = table_body.find_all("tr")
-cells = rows[1].find_all("td")
+    column_headers = soup.find("thead").text.splitlines()[2:-1]
+    table_body = soup.find("tbody")
+    rows = table_body.find_all("tr")
+    #this was checking the frist team's data to make sure it grabs everything in the correct format
+    # cells = rows[1].find_all("td")
 
-print(list(headers)[2:])
-print([cell.get_text(strip=True) for cell in cells])
+    all_team_stats = []
+    
+    for row in rows:
+        cells = row.find_all("td")
+        #if there was a blank for or seperator row, this would skip it
+        if not cells:
+            continue
+
+        #special case because table has formal team name and shortened name. Only want shortened name
+        team_data = []
+        team_cell = cells[0]
+        short_team_name = team_cell.find("div", class_="d3-o-club-fullname")
+        team_data.append(short_team_name.get_text(strip=True))
+
+        #grab the rest of the stats
+        for cell in cells[1:]:
+            team_data.append(cell.get_text(strip=True))
+        
+        all_team_stats.append(team_data)
+    
+    return column_headers, all_team_stats
+
+print(get_stats(url, headers))
+
+column_headers, team_stats = get_stats(url, headers)
+
+def create_pandas_df(data, headers):
+    df = pd.DataFrame(data=data, columns=headers)
+    print(df.head())
+
+create_pandas_df(team_stats, column_headers)
+
+print(create_pandas_df(team_stats, column_headers))
